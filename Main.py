d494d5c637aaff5d49c21d665dfd56367b7f49df
BingSanBot <78294876+BingSanBot@users.noreply.github.com>
2021-02-07 15:20:12 +0900

    Update Main.py

--- Main.py
+++ Main.py
@@ -30,7 +30,7 @@ async def on_message(message):
 
 
     if message.content == "!명령어":
-        embed = discord.Embed(color=discord.Colour.red(), title="빙산봇 명령어 설명입니다", descriptor="개발중인 명령어도 포함됩니다")
+        embed = discord.Embed(color=discord.Color.red(), title="빙산봇 명령어 설명입니다", descriptor="개발중인 명령어도 포함됩니다")
         embed.add_field(name="!테스트", value="빙산봇의 현재상태를 나타냅니다",inline=True)
         embed.add_field(name="!내정보", value="나의 디스코드 정보를 나타냅니다",inline=True)
         embed.set_footer(text="Made By. Sehyun")
